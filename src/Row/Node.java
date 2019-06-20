57a6a9c32a7b7876548029f762ebdbb74e391310
Yolo <reyg.61189@gmail.com>
2019-06-20 12:29:51 -0600

    It is first version of List_Row, it has method add, empty,

--- src/Row/Node.java
+++ src/Row/Node.java
@@ -4,8 +4,24 @@ public class Node {
     String data;
     Node next;
 
-    public Node(){
-        data = null;
-        next = null;
+    public Node(String data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
