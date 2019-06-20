48bea3fa90d8210c16853ef00db5aa822e0799f1
Yolo <reyg.61189@gmail.com>
2019-06-20 12:44:01 -0600

    It has the new class when user writes names in the Row.

---
+++ src/Row/MainRow.java
@@ -0,0 +1,10 @@
+package Row;
+
+public class MainRow {
+    public static void main(String [] args){
+        ListRow miListRow = new ListRow();
+        miListRow.addNode("Raul");
+        miListRow.addNode("Brandon");
+        miListRow.printRow();
+    }
+}
